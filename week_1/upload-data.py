13342b35368e70f786ed8b45cb27a73aacdf9746
Amanda Smith <amandasmith@Amandas-MacBook-Air.local>
2022-03-27 17:34:17 +0100

    add engine

--- week_1/upload-data.py
+++ week_1/upload-data.py
@@ -1,8 +1,10 @@
 import pandas as pd
+from sqlalchemy import create_engine
 
-
-df = pd.read_csv("yellow_tripdata_2021-01.csv", nrows=1000)
+df = pd.read_csv("week_1/yellow_tripdata_2021-01.csv", nrows=1000)
 
 print(df.head())
 
-print(pd.io.sql.get_schema(df, name="yellow_trip_data"))
+engine = create_engine("postgresql://root:root@localhost:5432/ny_taxi")
+
+print(pd.io.sql.get_schema(df, name="yellow_trip_data", con=engine))
